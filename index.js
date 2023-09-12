7ce14bc65206f52bad94dd60327db8a767c13e82
serkancan <serkancan@DESKTOP-9CS0Q1K>
2023-09-12 10:57:20 +0300

    sunucuya gelen verileri okuma

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const data = require("./data.js");
 
 const server = express();
+server.use(express.json());
 
 server.get("/", (req, res) => {
   res.send("express ten merhaba");
@@ -12,6 +13,13 @@ server.get("/aktorler", (req, res) => {
 });
 
 server.get("/aktorler/:aktor_id", (req, res) => {
+  // query,params,body haricinde header... gibi özelliklerde var   tam tersi res içinde status data gibi değerler olabilir.
+  //http://127.0.0.1:5000/aktorler/1?isim=kemal&soyisim=sunal&film_turu=komedi
+  //console.log(req.query);
+  ////http://127.0.0.1:5000/aktorler/1
+  //console.log(req.params);
+  //bir body gönderirsen eğer json bir veri mesela
+  //console.log(req.body)    ile değer hem get hem post metodlarından çekilebilir
   const { aktor_id } = req.params;
   const aktor = data.find((aktor) => aktor.id === parseInt(aktor_id));
   if (aktor) {
