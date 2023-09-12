618fc1c8d62c16bae347c2f1ab12f033da68365d
serkancan <serkancan@DESKTOP-9CS0Q1K>
2023-09-12 10:41:17 +0300

    add nodemon

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ server.get("/aktorler/:aktor_id", (req, res) => {
   if (aktor) {
     res.status(200).json(aktor);
   } else {
-    res.status(404).send("aradiginiz aktor bulunamadi");
+    res.status(404).send("aradiginiz aktor bulunamadiiiii");
   }
 });
 
